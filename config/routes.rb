44f9fa96173bc1ed9ffa28a1e77f13dc19d91fa4
alexheil <aheil4250@gmail.com>
2017-12-03 13:20:38 -0500

    seventh

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
+  get 'privacy' => 'static_pages#privacy'
+  get 'terms' => 'static_pages#terms'
+  get 'about' => 'static_pages#about'
+
   devise_for :admins, controllers: { sessions: "admins/sessions", passwords: "admins/passwords", registrations: "admins/registrations", confirmations: "admins/confirmations",  unlocks: "admins/unlocks"}
 
   devise_scope :admin do
