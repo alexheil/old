7c001fef8839b7373149190fa299a3042eb05889
alexheil <aheil4250@gmail.com>
2017-12-05 19:10:47 -0500

    fourteenth

--- app/models/email.rb
+++ app/models/email.rb
@@ -3,7 +3,9 @@ class Email < ApplicationRecord
   validates :email, presence: true, uniqueness: true, length: { maximum: 75 }, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
 
   def self.email_blast
-    EmailMailer.email_blast(email).deliver_now
+    Email.find_each do |email|
+      EmailMailer.email_blast(email).deliver_now
+    end
   end
 
 end
